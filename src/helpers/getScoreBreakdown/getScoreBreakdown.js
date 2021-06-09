cb21977c93c9a38e5244d337fac05fd74b0ae0f0
Danielle O'Sullivan <danielle.oh.es@gmail.com>
2021-06-09 23:44:24 +0100

    Adjust ratings from 2-11 to 1-10

--- src/helpers/getScoreBreakdown/getScoreBreakdown.js
+++ src/helpers/getScoreBreakdown/getScoreBreakdown.js
@@ -2,7 +2,7 @@ export default function getScoreBreakdown(scoresObject) {
   let scoreData = []
   for (var i = 1; i <= Object.keys(scoresObject).length; i++) {
     const numberOfVotes = scoresObject[i].votes
-    scoreData.push({Score: i+1, Votes: numberOfVotes})
+    scoreData.push({Score: i, Votes: numberOfVotes})
   }
   return scoreData
 }
